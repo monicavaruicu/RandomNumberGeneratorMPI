1d9759414bb2c5701ef6a3dcf6e9b46b7b50a8a2
Monica Văruicu <107719830+monicavaruicu@users.noreply.github.com>
2024-05-09 10:29:24 +0300

    refactor: remove unused code

--- RandomNumberGeneratorMPI/RandomNumberGeneratorMPI/RandomNumberGeneratorMPI.cpp
+++ RandomNumberGeneratorMPI/RandomNumberGeneratorMPI/RandomNumberGeneratorMPI.cpp
@@ -9,7 +9,7 @@ unsigned long long midSquareRandom(unsigned long long seed, int iterations) {
     unsigned long long result = 0;
     for (int i = 0; i < iterations; ++i) {
         num *= num;
-        num /= 100; // extrage cifrele din mijloc
+        num /= 100;
         result = result * 10 + (num % 10);
     }
     return result;
@@ -33,8 +33,8 @@ int main(int argc, char** argv) {
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-    unsigned long long seed = std::time(0) + rank; // seed-ul pentru fiecare proces MPI
-    int iterations = 5; // numărul de iterații pentru fiecare proces
+    unsigned long long seed = std::time(0) + rank;
+    int iterations = 5;
 
     if (rank == 0) {
         std::cout << "Metoda pătratului mijlociu:\n";
@@ -56,7 +56,7 @@ int main(int argc, char** argv) {
 
     unsigned long long a = 1664525;
     unsigned long long c = 1013904223;
-    unsigned long long m = 4294967296; // 2^32
+    unsigned long long m = 4294967296;
     unsigned long long linearCongruentialResult = linearCongruentialRandom(seed, a, c, m, iterations);
     MPI_Gather(&linearCongruentialResult, 1, MPI_UNSIGNED_LONG_LONG, &results[0], 1, MPI_UNSIGNED_LONG_LONG, 0, MPI_COMM_WORLD);
 
